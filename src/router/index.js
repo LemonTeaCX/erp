a11db479df0a63a65e4d6f6a037e82be319d0b67
lemon <1406428663@qq.com>
2019-02-28 17:27:14 +0800

    Create index.js

---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import Index from '../pages/index'
+import Account from '../pages/system/account'
+import Auth from '../pages/system/auth'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      component: Index,
+      children: [
+      	{
+		    	path: '/system/account',
+		      component: Account
+		    },
+		    {
+		    	path: '/system/auth',
+		      component: Auth
+		    }
+      ]
+    },
+  ]
+})
