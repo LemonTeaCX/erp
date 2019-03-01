1e375d17b1c608bad22e46f3d701f0aaed99f3a2
lemon <1406428663@qq.com>
2019-03-01 17:26:16 +0800

    Update index.js

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,11 +1,22 @@
 import Mock from 'mockjs';
+import Util from '../util/util';
 
 Mock.setup({
     timeout: 1000
-})
+});
 
-Mock.mock('http://localhost:8080/api/getMenu', 'get', {
+let { copyJson } = new Util();
+let BASE_URL = 'http://localhost:8080';
+let resWrap = {
 	"code": 0,
+  "data": {},
+	"msg": "success"
+};
+let mergeRes = (json, ...moreJson) => {
+	return Object.assign(copyJson(resWrap), json, ...moreJson);
+};
+
+Mock.mock(`${BASE_URL}/api/getMenu`, 'get', mergeRes({
   "data": [
 		{
 	    "id": 1,
@@ -36,6 +47,22 @@ Mock.mock('http://localhost:8080/api/getMenu', 'get', {
 			"menu": "菜单3",
 			"icon": "el-icon-document"
 		}
-	],
-  "msg": "success"
-});
+	]
+}));
+
+Mock.mock(`${BASE_URL}/api/getAccountList`, 'get', mergeRes({
+  "data": {
+		"list|3-20": [
+			{
+		    "id|+1": 1,
+				"name": "@cname",
+				"email": "@email",
+				"status|1": false,
+				"auth": ["权限1", "权限2", "权限3"],
+				"role|1": ["超级管理员", "普通用户"],
+				"remark": "@sentence"
+			}
+		],
+		"total|20-100": 1 
+	}
+}));
