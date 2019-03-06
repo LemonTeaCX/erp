75d19749737b25df876e3985ed46a2c30f06321e
lemon <1406428663@qq.com>
2019-03-06 17:26:42 +0800

    Update system.js

--- src/api/system.js
+++ src/api/system.js
@@ -1,5 +1,7 @@
 import ajax from '../util/ajax';
 
-export const getAccountList = () => ajax({
-	url: '/api/getAccountList'
+export const getAccountList = (parms) => ajax({
+	url: '/api/getAccountList',
+	method: 'post',
+	data: parms
 });
