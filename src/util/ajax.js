36b004a072cc5fd2858dbc196194092b93ee4458
lemon <1406428663@qq.com>
2019-03-06 17:27:12 +0800

    Update ajax.js

--- src/util/ajax.js
+++ src/util/ajax.js
@@ -1,19 +1,16 @@
 import axios from 'axios';
+import Qs from 'qs';
 import LoadingUtil from './loading';
 
 let loadingUtil = new LoadingUtil();
 
-axios.defaults.baseURL ='http://localhost:8080';
+axios.defaults.timeout = 30000;
+axios.defaults.baseURL ='http://localhost:3000';
+axios.defaults.withCredentials = true;
 
 //http request 拦截器
 axios.interceptors.request.use(
   config => {
-    config.headers = {
-      'Content-Type': 'applicaiton/json'
-    }
-    // const token = getCookie('名称');
-    // if(token) config.params = {'token':token}
-    
     if (config.loading) loadingUtil.startLoading();
     return config;
   },
@@ -26,24 +23,24 @@ axios.interceptors.request.use(
 axios.interceptors.response.use(
   response => {
     loadingUtil.endLoading();
-    /*if(response.data.errCode ==2){
-      router.push({
-        path:"/login",
-        querry:{redirect:router.currentRoute.fullPath}//从哪个页面跳转
-      })
-    }*/
     return response.data;
   },
   error => {
-    return Promise.reject(error)
+    loadingUtil.endLoading();
+    return Promise.reject(error);
   }
 );
 
 export default function ajax(options) {
 	return new Promise((resolve, reject) => {
 		let config = Object.assign({
-			method: 'get'
+			method: 'get',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
 		}, options);
+
+    config.data = Qs.stringify(config.data);
 
 		axios(config)
 		.then(response => {
