035d8ff32cb1cfabacd65d575c564f09c429f4a5
lemon <1406428663@qq.com>
2019-03-07 17:32:59 +0800

    Update util.js

--- src/util/util.js
+++ src/util/util.js
@@ -1,6 +1,20 @@
 export default class Util {
 	constructor(options) {}
-	copyJson(json) {
+	copyJson(json = {}) {
 		return JSON.parse(JSON.stringify(json));
+	}
+	getCookie(name = '') {
+		let cookies = document.cookie.split(';');
+		for (let i = cookies.length - 1; i >= 0; i--) {
+			let arr = cookies[i].split('=');
+			if (arr[0] === name) return arr[1];
+		}
+		return '';
+	}
+	setCookie(cname, cvalue, exdays) {
+		var date = new Date();
+	  date.setTime(date.getTime()+(exdays*24*60*60*1000));
+	  var expires = "expires="+date.toGMTString();
+	  document.cookie = cname + "=" + cvalue + "; " + expires;
 	}
 }
