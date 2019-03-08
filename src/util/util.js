1660b7616dd5324a61979a333eb8adb3ee306b66
lemon <1406428663@qq.com>
2019-03-08 17:30:27 +0800

    Update util.js

--- src/util/util.js
+++ src/util/util.js
@@ -6,15 +6,16 @@ export default class Util {
 	getCookie(name = '') {
 		let cookies = document.cookie.split(';');
 		for (let i = cookies.length - 1; i >= 0; i--) {
-			let arr = cookies[i].split('=');
-			if (arr[0] === name) return arr[1];
+			let cookie = cookies[i].trim();
+    	if (cookie.indexOf(name) === 0)
+    		return cookie.substring(name.length + 1, cookie.length);
 		}
 		return '';
 	}
 	setCookie(cname, cvalue, exdays) {
-		var date = new Date();
+		let date = new Date();
 	  date.setTime(date.getTime()+(exdays*24*60*60*1000));
-	  var expires = "expires="+date.toGMTString();
-	  document.cookie = cname + "=" + cvalue + "; " + expires;
+	  let expires = 'expires='+date.toGMTString();
+	  document.cookie = cname + '=' + cvalue + '; ' + expires;
 	}
 }
