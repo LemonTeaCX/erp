3be68ca2ba2ac714ec002872ddf66eb38a4b6636
lemon <1406428663@qq.com>
2019-03-12 17:29:38 +0800

    Update actions.js

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,10 +1,14 @@
 import {
 	SET_MENU,
-	SET_USER_INFO
+	SET_USER_INFO,
+	SET_AUTH_LIST,
+	SET_ROLE_LIST
 } from './mutation-types';
 
 import {
-	getMenu
+	getMenu,
+	getAuth,
+	getRole
 } from '../api'
 
 export default {
@@ -15,5 +19,13 @@ export default {
 	setUserInfo({commit}, userInfo) {
 		window.localStorage.setItem('userInfo', JSON.stringify(userInfo) || '');
 		commit(SET_USER_INFO, userInfo);
-	}
+	},
+	async setAuthList({commit}) {
+		const result = await getAuth();
+		commit(SET_AUTH_LIST, result.data);
+	},
+	async setRoleList({commit}) {
+		const result = await getRole();
+		commit(SET_ROLE_LIST, result.data);
+	},
 }
